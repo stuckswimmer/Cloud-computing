1e57ff66e4dbf323af0f3de895c654575e1193d6
stuckswimmer <52112722+stuckswimmer@users.noreply.github.com>
2020-02-16 19:49:38 +0000

    Update and rename Tuple.java to KVPair.java

---
+++ KVPair.java
@@ -0,0 +1,35 @@
+
+public class KVPair {
+	private String key;
+    private Object value;
+
+    /**
+     * constructor for mapper output (key-value pair)
+     *
+     * @param key   key
+     * @param value value
+     */
+    public KVPair(String key,Object value){
+        this.key = key;
+        this.value = value;
+    }
+
+	/**
+     * Get the key
+     *
+     * @return
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * Get the Value
+     *
+     * @return
+     */
+    public Object getValue() {
+        return value;
+    }
+
+}
